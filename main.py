52cd1ee4dc2e26bc2cd1d2e1d5a6d1538a32cb95
FriedTerminator <43394956+FriedTerminator@users.noreply.github.com>
2025-05-13 15:50:40 -0700

    maintaining 60 fps

--- main.py
+++ main.py
@@ -6,11 +6,20 @@ BLACK = (0,0,0)
 def main():
     pygame.init()
 
+    clock = pygame.time.Clock()
+    dt = 0
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
         screen.fill(BLACK)
         pygame.display.flip()
+
+        dt = clock.tick(60)/1000
 
 
 if __name__ == "__main__":
