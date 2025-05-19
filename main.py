28a2c30fa4e6b6078321d715c5189485bd63c055
FriedTerminator <43394956+FriedTerminator@users.noreply.github.com>
2025-05-19 12:48:00 -0700

    got the player triangle to move around

--- main.py
+++ main.py
@@ -9,20 +9,25 @@ y = SCREEN_HEIGHT/2
 
 def main():
     pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    updatables = pygame.sprite.Group()
+    drawables = pygame.sprite.Group()
+    Player.containers = (updatables, drawables)
 
     player = Player(x, y)
     clock = pygame.time.Clock()
     dt = 0
-
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
+        updatables.update(dt)
         screen.fill(BLACK)
-        player.draw(screen)
+        for item in drawables:
+            item.draw(screen)
         pygame.display.flip()
 
         dt = clock.tick(60)/1000
--- player.py
+++ player.py
@@ -1,5 +1,5 @@
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS
+from constants import *
 import pygame
 
 WHITE = (255,255,255)
@@ -19,3 +19,22 @@ class Player(CircleShape):
 
     def draw(self, screen):
         pygame.draw.polygon(screen, WHITE, self.triangle(), 2)
+
+    def rotate(self, dt):
+        self.rotation += PLAYER_ROTATION_SPEED * dt
+    
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            self.rotate(-dt)
+        if keys[pygame.K_d]:
+            self.rotate(dt)
+        if keys[pygame.K_w]:
+            self.move(dt)
+        if keys[pygame.K_s]:
+            self.move(-dt)
+
+    def move(self, dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED * dt
